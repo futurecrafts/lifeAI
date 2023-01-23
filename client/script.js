cefb1766186c8c1047b680d60058c3f21439578a
YB <yb@yb.com>
2023-01-23 10:52:03 +1000

    update the server url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://127.0.0.1:5000', {
+  const response = await fetch('https://lifeai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
